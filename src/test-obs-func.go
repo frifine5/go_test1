6c581279ce4afe5f8e193131e463633a4f12a48d
wangchengyu <wangchengyu@baiwang.com>
2019-11-12 18:15:35 +0800

    on download by input key

--- src/test-obs-func.go
+++ src/test-obs-func.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"obs"
 	"os"
+	"bufio"
 )
 
 var ak = "OM65GZAEBJQAX9EFZVAX"
@@ -19,24 +20,53 @@ var obsClient, _ = obs.New(ak, sk, endpoint, config)
 func main() {
 	println("start ...")
 
-	//download()
+	download()
 	//listBkt()
 
-	listObjInBkt("obs-dc-dzqz")
+	//listObjInBkt("obs-dc-dzqz")
 
 	println("... end.")
 }
 
+
 func download(){
+	var key string
+	fmt.Println("请输入要下载的obs文件名")
+	//使用os.Stdin开启输入流
+	in := bufio.NewScanner(os.Stdin)
+	if in.Scan() {
+		key = in.Text()
+		fmt.Println("input data = " + key)
+	} else {
+		fmt.Println("input err and exit")
+		return
+	}
+
 	input := &obs.GetObjectInput{}
 	input.Bucket = "obs-dc-dzqz"
-	input.Key = "objectname"
+	input.Key = key
 	// 下载对象
 	output, err := obsClient.GetObject(input)
 	if err == nil {
 		defer output.Body.Close()
 		// 获取对象自定义元数据
-		fmt.Printf("Metadata:%v\n", output.Metadata)
+		//fmt.Printf("Metadata:%v\n", output.Metadata)
+		p := make([]byte, 1024)
+		var readErr error
+		var readCount int
+		// 读取对象内容
+		for {
+			readCount, readErr = output.Body.Read(p)
+			if readCount > 0 {
+				//fmt.Printf("%s", p[:readCount])
+			}
+			obsWrite2( key, p)
+			if readErr != nil {
+				break
+			}
+		}
+		fmt.Printf("down %s finish\n", key)
+
 	} else if obsError, ok := err.(obs.ObsError); ok {
 		fmt.Printf("Code:%s\n", obsError.Code)
 		fmt.Printf("Message:%s\n", obsError.Message)
@@ -94,6 +124,17 @@ func obsWrite1(fp string, content string) {
 	defer fileObj.Close()
 	if _, err := fileObj.WriteString(content + "\n"); err == nil {
 		fmt.Println("Successful writing to the file with os.OpenFile and *File.WriteString method.")
+	}
+}
+func obsWrite2(fp string, ctx []byte) {
+
+	fileObj, err := os.OpenFile(fp, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644) //  |os.O_TRUNC
+	if err != nil {
+		fmt.Println("Failed to open the file", err.Error())
+		os.Exit(2)// 应用进程退出
+	}
+	defer fileObj.Close()
+	if _, err := fileObj.Write(ctx); err == nil {
 	}
 }
 
