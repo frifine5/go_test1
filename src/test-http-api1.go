82096cbf55620061fd358a0a217a10a3b4c30198
wangchengyu <wangchengyu@baiwang.com>
2020-04-26 09:37:13 +0800

    go api of http

--- src/test-http-api1.go
+++ src/test-http-api1.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	//第一个参数是接口名，第二个参数 http handle func
-	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/a", helloWorld) // 使用‘/’或匹配所有
+	http.HandleFunc("/abc", helloWorld2)
 	//服务器要监听的主机地址和端口号
 	http.ListenAndServe(":8082", nil)
 }
@@ -18,6 +19,13 @@ func helloWorld(rw http.ResponseWriter, req *http.Request) {
 	fmt.Println("get on time: "+ (time.Now()).String())
 
 	// 返回字符串 "Hello world"
-	fmt.Fprint(rw, "Hello world")
+	fmt.Fprint(rw, " Hello world")
+
+}
+func helloWorld2(rw http.ResponseWriter, req *http.Request) {
+	fmt.Println("get on time 2: "+ (time.Now()).String())
+
+	// 返回字符串 "Hello world"
+	fmt.Fprint(rw, "Hello world - in url 2")
 
 }
--- src/test-http-api2.go
+++ src/test-http-api2.go
@@ -8,11 +8,7 @@ import (
 	"strings"
 )
 
-type Result struct {
-	ErrorCode int  `json:"errorCode"`
-	Message string `json:"message"`
-	Data  string `json:"data"`
-}
+
 
 func main() {
 	//第一个参数是接口名，第二个参数 http handle func
@@ -25,26 +21,24 @@ func main() {
 func getMockData(rw http.ResponseWriter, req *http.Request) {
 	fmt.Println("get on time: "+ (time.Now()).String())
 
+	s, _ := ioutil.ReadAll(req.Body) //把  body 内容读入字符串 s
+	fmt.Printf(  "%s\n", s)        //在返回页面中显示内容。
+
+
 	path := req.RequestURI
 	path = "/mnt" + path
 
-	rs := Result{
+	result:="加载mock数据失败"
 
-	}
 	if contents, err := ioutil.ReadFile(path); err == nil{
-		result:= strings.Replace(string(contents), "\n", "", 1)
-		fmt.Println("Use ioutil.ReadFile to read a file:\n", result)
-		rs.ErrorCode = 0
-		rs.Message = "ok"
-		rs.Data = result
+		result= strings.Replace(string(contents), "\n", "", 1)
+		//fmt.Println("Use ioutil.ReadFile to read a file:\n", result)
 	}else{
 		fmt.Println("Use ioutil.ReadFile >>> null")
-		rs.ErrorCode = -1
-		rs.Message = "加载mock数据失败"
 	}
 
 
 	// 返回字符串 "Hello world"
-	fmt.Fprint(rw, rs)
+	fmt.Fprint(rw, result)
 
 }
